7e5136b8c24b4880fb8ed4f6df79f0c98fb9e116
John Jeffrey PANINGBATAN <53562492+Muneo42@users.noreply.github.com>
2021-01-12 17:50:07 +0100

    Update data_base.sql

--- srcs/data_base.sql
+++ srcs/data_base.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE wordpress CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 GRANT ALL PRIVILEGES ON wordpress.* TO username @'localhost' IDENTIFIED BY 'password';
 
-CREATE USER 'selgrabl' @'localhost' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON *.* TO  'selgrabl' @'localhost' WITH GRANT OPTION;
+CREATE USER 'jopaning' @'localhost' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON *.* TO  'jopaning' @'localhost' WITH GRANT OPTION;
